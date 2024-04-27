2417eaac6105549307a912448db1dfe12b634931
ZeynepA <zeyneppaytop@gmail.com>
2024-04-28 01:18:49 +0300

    thirth commit

---
+++ cypress/e2e/homeworks/customCommands.cy.js
@@ -0,0 +1,35 @@
+/// <reference types="cypress" />
+/*Test Case1: Negatif Login Test
+1. https://www.automationexercise.com/login sayfasina gidin
+2. Email alanina gecersiz bir email girin
+3. Parola alanina gecersiz bir parola girin
+4. Login butonuna tiklayin
+5. Hata mesajini dogrulayin
+
+Test Case2: Pozitif Login Test 
+1. https://www.automationexercise.com/login sayfasina gidin
+2. Email alanina gecerli bir email girin
+3. Parola alanina gecerli bir parola girin
+4. Login butonuna tiklayin
+5. Kullanici adini dogrulayin
+6. Logout butonuna tiklayin
+7. Logout isleminden sonra Login butonunu dogrulayin
+
+Bu Case'leri yazarken asagidaki adimlari uygulayiniz:
+1. Page Object Modeli uygulayin
+2. login ve logout icin custom commands olusturun
+3. Test runner'da email ve parolanin goruntulenmemesi icin gerekli komutu type() icine yazin
+
+Asagidaki bilgileri testlerde kullanabilirsiniz.
+Gecerli email: testclarusway@test.com
+Gecerli parola: test@12345 */
+
+describe('Custom Commands ile Login testleri', () => {
+    it('Test Case1: Negatif Login Test', () => {
+        
+    });
+
+    it('Test Case2: Pozitif Login Test', () => {
+        
+    });
+});
---
+++ cypress/e2e/homeworks/fixture.cy.js
@@ -0,0 +1,21 @@
+/// <reference types="cypress" />
+
+/*1. https://www.betterroaming.com/ adresini gidin
+2. Para birimi olarak Euro'yu seçin
+3. Choose your destination kısmına Turkey yazıp aratın
+4. İkinci planın aşağıdaki değerlere sahip olup olmadığını kontrol edin:
+Countries: Turkey
+Data: 5GB
+Valid: 30 DAYS
+Plan type: Data only
+Price 8,49 €
+Page Object Modeli uygulayın
+Custom Commands'ı uygulayın
+Verileri Kullanmak için Fixtures klasöründen faydalanın. */
+
+describe('Fixture kullanarak test etme', () => {
+    it('Fixture ile test', () => {
+        
+    });
+    
+});
