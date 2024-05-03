5ed526bc9da605eaca953d8faaac3b430bd616fc
ZeynepA <zeyneppaytop@gmail.com>
2024-05-03 21:58:29 +0300

    hooks added

---
+++ cypress/e2e/cypress-dersleri/cypressHooks.cy.js
@@ -0,0 +1,202 @@
+// 1234567890,
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+,
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+444444444444444444444Cae cviiiiiiiiiiiiiiiii s×öjzsö ö tçkzöypress Hooks Kullanim Ornekleri
+
+/// <reference types= "cypress" />
+
+import homepage from "../../support/pageObjects/homepage/homepage";
+
+describe('Cypress Hooks Kullanimi', () => {
+    before(() => {
+        cy.log('TEST BASLIYORRR!!!')
+    })
+
+    beforeEach(() => {
+        cy.on('uncaught:exception', (err, runnable) => { return false })
+        cy.log('Sayfa hatasiz acildi')
+        homepage.navigate()
+        cy.login3('deneme123@test.com', 'Deneme@12345', 'Login')      
+    });
+
+    after(() => {
+        cy.log('Test tamamlandi!!!')
+    });
+
+    afterEach(() => {
+        cy.log('IT BLOGU TAMAMLANDI!!!!')
+        cy.logout()
+    });
+
+    it('Sayfaya gidelim dogrulama yapalim', () => {
+        homepage.verifyUrl('automationtesting')
+        homepage.verifyTitle('My Account – Automation Practice Site')
+    });
+
+    it('Before kullanim ornegi', () => {
+        // Burada test senaryomuz kostu
+    });
+
+    it('BeforeEach Kullanim Ornegi', () => {
+        // Burada farkli bir test senaryomuz kostu!
+    });
+});
